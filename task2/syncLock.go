51e251836f140442062062591287aeccb6cd4a61
integrationex01 <legionubisoft@163.com>
2025-11-04 14:27:40 +0800

    修改锁使用的方案，应多协程公用一把锁

--- task2/syncLock.go
+++ task2/syncLock.go
@@ -10,16 +10,16 @@ import (
 // 1. 题目 ：编写一个程序，使用 sync.Mutex 来保护一个共享的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
 //    - 考察点 ： sync.Mutex 的使用、并发数据安全。
 
-func Count(n *int) {
-	var syncLock sync.Mutex
+func Count(n *int, syncLock *sync.Mutex) {
+	defer syncLock.Unlock()
 	syncLock.Lock()
 	*n++
-	syncLock.Unlock()
 }
 
 func SyncLockLearn() {
 	defer fmt.Println("主线程已退出")
 	n := 0
+	var syncLock sync.Mutex
 	var wg sync.WaitGroup
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
@@ -27,7 +27,7 @@ func SyncLockLearn() {
 			defer wg.Done()
 			fmt.Printf("协程%d执行中\n", i)
 			for range 1000 {
-				Count(&n)
+				Count(&n, &syncLock)
 			}
 		}()
 	}
